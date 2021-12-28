a8f2bc0a638a1c14f7e7d2c77d81fd89b8f43513
Vitor Miranda <vitor.miranda@enjoei.com.br>
2021-12-28 16:32:37 -0300

    :sparkles: (feature) add player animation

--- main.lua
+++ main.lua
@@ -7,6 +7,17 @@ Game = {
   background = nil
 }
 
+Player = {
+  velx = 1,
+  inGround = false,
+  animation = {},
+}
+Ground = {}
+Obstacle = {
+  velx = -50,
+  vely = 0,
+}
+
 -- Roda quando o jogo abre (Inicialização deve acontecer aqui)
 function love.load()
   -- the height of a meter our worlds will be 64px
@@ -14,15 +25,6 @@ function love.load()
   love.physics.setMeter(meter)
   -- create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
   World = love.physics.newWorld(0, 9.81*meter, true)
-  Player = {
-    velx = 1,
-    inGround = false
-  }
-  Ground = {}
-  Obstacle = {
-    velx = -50,
-    vely = 0,
-  }
 
   love.window.setMode(
     Game.width * Game.scale,
@@ -44,11 +46,13 @@ function love.load()
   Player.fixture:setUserData("Player")
 
   Obstacle.body = love.physics.newBody(World, Game.width, Game.height, "dynamic")
-  Obstacle.shape = love.physics.newRectangleShape(0, 0, 10, 15) -- 10x15 tamanho do obstaculo
+  Obstacle.shape = love.physics.newRectangleShape(Obstacle.image:getWidth(), Obstacle.image:getHeight())
   Obstacle.fixture = love.physics.newFixture(Obstacle.body, Obstacle.shape, 5)
   Obstacle.fixture:setUserData("Obstacle")
 
   love.graphics.setBackgroundColor(1, 1, 1)
+
+  Player.animation = NewAnimation(love.graphics.newImage("assets/images/player-animation.png"), 16, 16, 1)
 end
 
 -- Roda a cada frame (Realizar update de estado aqui)
@@ -66,6 +70,11 @@ function love.update(dt)
 
   if Player.body:getX() < 1 then -- limimar para o game over
     Game.over = true
+  end
+
+  Player.animation.currentTime = Player.animation.currentTime + dt
+  if Player.animation.currentTime >= Player.animation.duration then
+    Player.animation.currentTime = Player.animation.currentTime - Player.animation.duration
   end
 end
 
@@ -86,12 +95,15 @@ function love.draw()
   RGBColor(208, 98, 36)
   love.graphics.polygon("fill", Ground.body:getWorldPoints(Ground.shape:getPoints()))
 
-  -- -- desenha o player na posição x e y
+  -- desenha o player na posição x e y
   RGBColor(255, 255, 255)
-  love.graphics.draw(Player.image, Player.body:getX(), Player.body:getY(), 0,  1, 1, Player.image:getWidth()/2, Player.image:getHeight()/2)
 
-  RGBColor(0, 0, 0)
-  love.graphics.polygon("fill", Obstacle.body:getWorldPoints(Obstacle.shape:getPoints()))
+  local spriteNum = math.floor(Player.animation.currentTime / Player.animation.duration * #Player.animation.quads) + 1
+
+  love.graphics.draw(Player.animation.spriteSheet, Player.animation.quads[spriteNum], Player.body:getX(), Player.body:getY(),  0,  1, 1, Player.image:getWidth()/2, Player.image:getHeight()/2)
+
+  RGBColor(255, 255, 255)
+  love.graphics.draw(Obstacle.image, Obstacle.body:getX(), Obstacle.body:getY(), 0,  1, 1, Obstacle.image:getWidth()/2, Obstacle.image:getHeight()/2)
 end
 
 function love.keypressed(key)
@@ -107,9 +119,7 @@ function love.keypressed(key)
 
   -- Player
   if key == "up" then
-    if InGround() then
-      Player.body:applyLinearImpulse(0, -100)
-    end
+    Player:Jump()
   end
 end
 
@@ -122,12 +132,14 @@ end
 
 -- Funções auxiliares
 function LoadPlayerAssets()
-  Player.image = love.graphics.newImage("assets/images/player.png")
-  Player.image:setFilter("nearest", "nearest")
-  Player.width = Player.image:getWidth()
-  Player.height = Player.image:getHeight()
+  Player:SetImage("player.png")
 
   Game.background = love.graphics.newImage("assets/images/bg-wall-1.jpg")
+
+  Obstacle.image = love.graphics.newImage("assets/images/percent.png")
+  Obstacle.image:setFilter("nearest", "nearest")
+  Obstacle.width = Obstacle.image:getWidth()
+  Obstacle.height = Obstacle.image:getHeight()
 end
 
 function RGBColor(r, g, b)
@@ -145,6 +157,7 @@ end
 function BeginContact(a, b, coll)
   if a:getUserData() == "Ground" and b:getUserData() == "Player" then
     Player.inGround = true
+    Player:SetImage("player.png")
   end
 end
 
@@ -178,4 +191,41 @@ function PlayerWalk()
   if Player.body:getX() + Player.width > Game.width then
     Player.body:setX(Game.width - Player.width)
   end
+end
+
+function Player:Jump()
+  if Player:InGround() then
+    Player.body:applyLinearImpulse(0, -100)
+    Player:SetImage("player-jump.png")
+  end
+end
+
+function Player:InGround()
+  return Player.inGround
+end
+
+function Player:SetImage(image)
+  local imagePath = "assets/images/" .. image
+  Player.image = love.graphics.newImage(imagePath)
+  Player.image:setFilter("nearest", "nearest")
+
+  Player.width = Player.image:getWidth()
+  Player.height = Player.image:getHeight()
+end
+
+function NewAnimation(image, width, height, duration)
+  local animation = {}
+  animation.spriteSheet = image;
+  animation.quads = {};
+
+  for y = 0, image:getHeight() - height, height do
+      for x = 0, image:getWidth() - width, width do
+          table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+      end
+  end
+
+  animation.duration = duration or 1
+  animation.currentTime = 0
+
+  return animation
 end
