5fc692580fd9b69d37cabf4cd51a28458f5047f9
Vitor Miranda <vitor.miranda@enjoei.com.br>
2021-12-28 12:12:15 -0300

    :recycle: (refactor) physics

--- main.lua
+++ main.lua
@@ -1,53 +1,67 @@
 Game = {
   width = 320,
   height = 180,
-  scale = 4,
+  scale = 5,
   name = "Enjuway",
-  gravity = 0.1,
   over = false,
-}
-
-Player = {
-  x = 40,
-  y = 0,
-  vely = 0,
-  impulse = -2.5,
-}
-
-Obstacle = {
-  x = Game.width - 8,
-  y = Game.height - 16,
-  width = 8,
-  height = 16,
-  velx = -1,
 }
 
 -- Roda quando o jogo abre (Inicialização deve acontecer aqui)
 function love.load()
+  -- the height of a meter our worlds will be 64px
+  local meter  = 18
+  love.physics.setMeter(meter)
+  -- create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
+  World = love.physics.newWorld(0, 9.81*meter, true)
+
+  Player = {
+    velx = 1,
+  }
+  Ground = {}
+  Obstacle = {
+    velx = -50,
+    vely = 0,
+  }
+
   love.window.setMode(
     Game.width * Game.scale,
     Game.height * Game.scale
   )
+
   love.window.setTitle(Game.name)
 
   LoadPlayerAssets()
+
+  Ground.body = love.physics.newBody(World, 0, Game.height, "static")
+	Ground.shape = love.physics.newRectangleShape(Game.width * Game.scale, 5)
+	Ground.fixture = love.physics.newFixture(Ground.body, Ground.shape)
+
+  Player.body = love.physics.newBody(World, 50, 5, "dynamic") -- player começa caindo
+	Player.shape = love.physics.newRectangleShape(Player.image:getWidth(), Player.image:getHeight())
+  Player.fixture = love.physics.newFixture(Player.body, Player.shape)
+
+  Obstacle.body = love.physics.newBody(World, Game.width, Game.height, "dynamic")
+  Obstacle.shape = love.physics.newRectangleShape(0, 0, 10, 15) -- 10x15 tamanho do obstaculo
+  Obstacle.fixture = love.physics.newFixture(Obstacle.body, Obstacle.shape, 5)
+
+  love.graphics.setBackgroundColor(0.41, 0.53, 0.97)
 end
 
 -- Roda a cada frame (Realizar update de estado aqui)
-function love.update()
-  Player.vely = Player.vely + Game.gravity -- variação da velocidade
-  Player.y = Player.y + Player.vely -- variação da posição
-  if Player.y > Game.height - Player.height then
-    PutInGround(Player)
+function love.update(dt)
+  World:update(dt)
+  World:setCallbacks(BeginContact, EndContact, PreSolve, PostSolve)
+
+  Obstacle.body:setLinearVelocity(Obstacle.velx, Obstacle.vely)
+
+  PlayerWalk()
+
+  if Obstacle.body:getX() < 0 then
+    Obstacle.body:setX(Game.width)
   end
 
-  Obstacle.x = Obstacle.x + Obstacle.velx
-  if Obstacle.x < 0 then
-    Obstacle.x = Game.width
-  end
-
-  if HasCollision(Player, Obstacle) then
-    GameOver()
+  if Player.body:getX() < 1 then -- limimar para o game over
+    Game.over = true
   end
 end
 
@@ -61,16 +75,16 @@ function love.draw()
     return
   end
 
-  -- definimos a cor branca
-  RGBColor(255, 255, 255)
-  love.graphics.rectangle("fill", 0, 0, Game.width, Game.height)
+  -- desenha o chão
+  RGBColor(0, 255, 0)
+  love.graphics.polygon("fill", Ground.body:getWorldPoints(Ground.shape:getPoints()))
 
   -- desenha o player na posição x e y
-  love.graphics.draw(Player.image, Player.x, Player.y)
+  RGBColor(255, 255, 255)
+  love.graphics.draw(Player.image, Player.body:getX(), Player.body:getY(), Player.body:getAngle(),  1, 1, Player.image:getWidth()/2, Player.image:getHeight()/2)
 
-  -- desenha o obstáculo na posição x e y
   RGBColor(0, 0, 0)
-  love.graphics.rectangle("fill", Obstacle.x, Obstacle.y, Obstacle.width, Obstacle.height)
+  love.graphics.polygon("fill", Obstacle.body:getWorldPoints(Obstacle.shape:getPoints()))
 end
 
 function love.keypressed(key)
@@ -86,9 +100,8 @@ function love.keypressed(key)
 
   -- Player
   if key == "up" then
-    -- o pulo pode iniciar se o avatar estiver no chão
-    if Game.height - Player.height == Player.y then
-      Player.vely = Player.impulse
+    if InGround() then
+      Player.body:applyLinearImpulse(0, -100)
     end
   end
 end
@@ -112,18 +125,44 @@ function RGBColor(r, g, b)
   love.graphics.setColor(r/255, g/255, b/255)
 end
 
-function PutInGround(obj)
-  obj.y = Game.height - obj.height
-  obj.vely = 0
-end
-
-function HasCollision(obj1, obj2)
-  return obj1.x < obj2.x + obj2.width and
-    obj1.x + obj1.width > obj2.x and
-    obj1.y < obj2.y + obj2.height and
-    obj1.y + obj1.height > obj2.y
-end
-
 function GameOver()
   Game.over = true
+end
+
+function InGround()
+  return Player.body:getY() > Ground.body:getY() - 11 -- 11 espaço em pixels entre o chão e o player
+end
+
+function BeginContact(a, b, coll)
+
+end
+
+function EndContact(a, b, coll)
+
+end
+
+function PreSolve(a, b, coll)
+
+end
+
+function PostSolve(a, b, coll, normalimpulse, tangentimpulse)
+
+end
+
+function PlayerWalk()
+  if love.keyboard.isDown("left") then
+    Player.body:setX(Player.body:getX() - Player.velx)
+  end
+
+  if love.keyboard.isDown("right") then
+    Player.body:setX(Player.body:getX() + Player.velx)
+  end
+
+  if Player.body:getX() < 0 then
+    Player.body:setX(0)
+  end
+
+  if Player.body:getX() + Player.width > Game.width then
+    Player.body:setX(Game.width - Player.width)
+  end
 end
